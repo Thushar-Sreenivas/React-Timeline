224659068b68c863195514532ef8db200f96d67b
Thushar Sreenivas <thushar@keyvalue.systems>
2020-05-16 13:34:09 +0530

    Added Moment for timeline

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import moment from 'moment'
+
+const events = [
+  { ts: "2017-09-17T12:22:46.587Z", text: "Logged in" },
+  { ts: "2017-09-17T12:21:46.587Z", text: "Clicked Home Page" },
+  { ts: "2017-09-17T12:20:46.587Z", text: "Edited Profile" },
+  { ts: "2017-09-16T12:22:46.587Z", text: "Registered" },
+  { ts: "2017-09-16T12:21:46.587Z", text: "Clicked Cart" },
+  { ts: "2017-09-16T12:21:46.587Z", text: "Clicked Cart" },
+  { ts: "2017-09-16T12:20:46.587Z", text: "Clicked Checkout" }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {events.map(event => (
+        <div>
+          <p>{moment(event.ts).format("DD MMM YYYY")}</p>
+          <p>{event.text}</p>
+        </div>
+      ))}
     </div>
   );
 }
